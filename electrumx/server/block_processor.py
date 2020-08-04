d9661583f34d7d7f92cfc6638ad14f9d15b2e58b
LitecoinPoS <support@litecoinpos.org>
2020-08-04 18:44:03 +0200

    up

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -123,6 +123,13 @@ class Prefetcher:
 
                 # Special handling for genesis block
                 if first == 0:
+                    header = self.coin.block_header(blocks[0], 0)
+                    self.logger.info(f'info 1 '
+                                     f'{header}')
+                    header_hex_hash = hash_to_hex_str(self.coin.header_hash(header))
+                    self.logger.info(f'info 2 '
+                                     f'{header_hex_hash}')
+
                     blocks[0] = self.coin.genesis_block(blocks[0])
                     self.logger.info(f'verified genesis block with hash '
                                      f'{hex_hashes[0]}')
